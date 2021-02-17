badc6eb649f66ca13b791c9d97950ad9a6b243be
maxwebsterkhan <max-webster-khan@zonedigital.com>
2021-02-17 21:25:54 +0000

    fixed navigation burger

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,41 +7,56 @@ import "./Navigation.css";
 
 const Navigation = () => {
   return (
-    <Navbar className="nav" bg="light" variant="light">
-      <NavLink className="linkItems" to="/">
-        Balu Technologies
-      </NavLink>
-      <Nav className="mr-auto"></Nav>
-      <NavDropdown
-        className="linkItems"
-        title="About us"
-        id="collasible-nav-dropdown"
-      >
-        <NavDropdown.Item className="linkItems">
-          <Nav.Link className="linkItems">
-            <NavLink activeClassName="active" to="/tech">
-              The Team
-            </NavLink>
-          </Nav.Link>
-        </NavDropdown.Item>
-        <NavDropdown.Item>
-          <Nav.Link className="linkItems">
-            <NavLink className="linkItems" activeClassName="active" to="/tech">
-              Tech
-            </NavLink>
-          </Nav.Link>
-        </NavDropdown.Item>
-      </NavDropdown>
-      <Nav.Link className="linkItems">
-        <NavLink className="linkItems" activeClassName="active" to="/contact">
-          Get In Touch
+    <Navbar className="nav" bg="light" expand="lg">
+      <Navbar.Brand>
+        <NavLink className="linkItems" to="/">
+          Balu Technologies
         </NavLink>
-      </Nav.Link>
-      <Nav.Link className="linkItems">
-        <NavLink className="linkItems" activeClassName="active" to="/projects">
-          Projects
-        </NavLink>
-      </Nav.Link>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto"> </Nav>
+
+        <NavDropdown
+          className="linkItems"
+          title="About us"
+          id="collasible-nav-dropdown"
+        >
+          <NavDropdown.Item className="linkItems">
+            <Nav.Link className="linkItems">
+              <NavLink activeClassName="active" to="/team">
+                The Team
+              </NavLink>
+            </Nav.Link>
+          </NavDropdown.Item>
+          <NavDropdown.Item>
+            <Nav.Link className="linkItems">
+              <NavLink
+                className="linkItems"
+                activeClassName="active"
+                to="/tech"
+              >
+                Tech
+              </NavLink>
+            </Nav.Link>
+          </NavDropdown.Item>
+        </NavDropdown>
+
+        <Nav.Link className="linkItems">
+          <NavLink
+            className="linkItems"
+            activeClassName="active"
+            to="/projects"
+          >
+            Projects
+          </NavLink>
+        </Nav.Link>
+        <Nav.Link className="linkItems">
+          <NavLink className="linkItems" activeClassName="active" to="/contact">
+            Get In Touch
+          </NavLink>
+        </Nav.Link>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
