dd8280989091420532dbf91527c4310ea3bcc150
ljefferies98 <ljefferies98@gmail.com>
2021-02-20 18:12:18 +0000

    add dynamic profile construction

---
+++ src/assets/profiles.js
@@ -0,0 +1,28 @@
+const profiles = [
+  {
+    "name": "Ben Jefferies | DEVOPS ENGINEER",
+    "description": "Ben is an experienced Software Engineer, who has had extensive exposure to Open Banking, along with a career that has covered Frontend, Backend and Mobile Development over the past 10 years. Driven by the shear love of software development, he takes pride in providing the best solution to the problems he faces. As a Software Engineer, he has completed many open-source project that are available to view on his GitHub. In his spare time, he loves to go cycling and enjoys flying his drone.\",",
+    "github": "https://github.com/benjefferies",
+    "linkedin": "https://www.linkedin.com/in/ben-jefferies-0bb1b24b/"
+  },
+  {
+    "name": "Jack Jefferies | SOFTWARE ENGINEER",
+    "description": "Ben is an experienced Software Engineer, who has had extensive exposure to Open Banking, along with a career that has covered Frontend, Backend and Mobile Development over the past 10 years. Driven by the shear love of software development, he takes pride in providing the best solution to the problems he faces. As a Software Engineer, he has completed many open-source project that are available to view on his GitHub. In his spare time, he loves to go cycling and enjoys flying his drone.\",",
+    "github": "https://github.com/benjefferies",
+    "linkedin": "https://www.linkedin.com/in/ben-jefferies-0bb1b24b/"
+  },
+  {
+    "name": "Lewis Jefferies | SOFTWARE ENGINEER",
+    "description": "Ben is an experienced Software Engineer, who has had extensive exposure to Open Banking, along with a career that has covered Frontend, Backend and Mobile Development over the past 10 years. Driven by the shear love of software development, he takes pride in providing the best solution to the problems he faces. As a Software Engineer, he has completed many open-source project that are available to view on his GitHub. In his spare time, he loves to go cycling and enjoys flying his drone.\",",
+    "github": "https://github.com/benjefferies",
+    "linkedin": "https://www.linkedin.com/in/ben-jefferies-0bb1b24b/"
+  },
+  {
+    "name": "Max Webster-Khan | FRONTEND ENGINEER",
+    "description": "Ben is an experienced Software Engineer, who has had extensive exposure to Open Banking, along with a career that has covered Frontend, Backend and Mobile Development over the past 10 years. Driven by the shear love of software development, he takes pride in providing the best solution to the problems he faces. As a Software Engineer, he has completed many open-source project that are available to view on his GitHub. In his spare time, he loves to go cycling and enjoys flying his drone.\",",
+    "github": "https://github.com/benjefferies",
+    "linkedin": "https://www.linkedin.com/in/ben-jefferies-0bb1b24b/"
+  }
+]
+
+export default profiles;
