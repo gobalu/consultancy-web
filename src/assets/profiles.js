47f77c83d56486ba4629a0437c0a940d9e6db330
Ben Jefferies <benjefferies@echosoft.uk>
2021-02-22 12:11:07 +0000

    Cut down bens bio

--- src/assets/profiles.js
+++ src/assets/profiles.js
@@ -1,7 +1,7 @@
 const profiles = [
   {
     "name": "Ben Jefferies | DEVOPS ENGINEER",
-    "description": "Ben is an experienced Software Engineer, who has had extensive exposure to Open Banking, along with a career that has covered Frontend, Backend and Mobile Development over the past 10 years. Driven by the shear love of software development, he takes pride in providing the best solution to the problems he faces. As a Software Engineer, he has completed many open-source project that are available to view on his GitHub. In his spare time, he loves to go cycling and enjoys flying his drone.",
+    "description": "Ben has 10 years of experience across front end and backend development as well as automated testing and more recently DevOps. As a Software Engineer, he has completed many open-source project that are available to view on his GitHub. In his spare time, he loves to cycle, snow ski, waterski and fly his drone.",
     "github": "https://github.com/benjefferies",
     "linkedin": "https://www.linkedin.com/in/ben-jefferies-0bb1b24b/",
     "strava": "https://www.strava.com/athletes/18464706/",
