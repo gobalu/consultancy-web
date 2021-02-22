1a422cab120d0b3c7453450d2588751eae772c02
Ben Jefferies <benjefferies@echosoft.uk>
2021-02-22 11:30:02 +0000

    Use HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 
