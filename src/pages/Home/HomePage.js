bb8ac415e9046c9e448097758f3db4dbff32c2a2
maxwebsterkhan <max-webster-khan@zonedigital.com>
2021-02-27 17:00:21 +0000

    remade homepage

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -2,107 +2,171 @@ import "./HomePage.css";
 
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
-import Image from "react-bootstrap/Image";
 import Jumbotron from "react-bootstrap/Jumbotron";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Row from "react-bootstrap/Row";
 import bgimage from "./Assets/bgimage.jpg";
+import { NavLink } from "react-router-dom";
 
-const HomePage = () => (
-  <Container className="plr0" fluid>
-    <Container fluid className="pbottom125 plr0">
-      <Jumbotron
-        className="height"
-        fluid
-        style={{
-          backgroundImage: `url(${bgimage})`,
-          backgroundSize: "cover",
-        }}
-      >
-        <Container fluid className="centerText">
-          <Row className="justify-content-md-center">
-            <Col md="auto">
-              <h2 className="white centerText">
-                Innovitive Start Up Solutions
-              </h2>
-            </Col>
-          </Row>
-          <Container className="heroText" fluid>
-            <Row>
-              <Col className="ptop50">
-                <p>
-                  We partner with a fantastic variety of clients. Using cutting
-                  edge technology, specialist skills and a passion for success,
-                  we produce software experiences that look great and deliver on
-                  their goals.
-                </p>
+const words = ["The Future", "Balu"];
+
+const HomePage = () => {
+  const [index, setIndex] = useState(0);
+  const [subIndex, setSubIndex] = useState(0);
+  const [blink, setBlink] = useState(true);
+  const [reverse, setReverse] = useState(false);
+
+  // typeWriter
+  useEffect(() => {
+    if (index === words.length) return;
+
+    if (
+      subIndex === words[index].length + 1 &&
+      index !== words.length - 1 &&
+      !reverse
+    ) {
+      setReverse(true);
+      return;
+    }
+
+    if (subIndex === 0 && reverse) {
+      setReverse(false);
+      setIndex((prev) => prev + 1);
+      return;
+    }
+
+    const timeout = setTimeout(() => {
+      setSubIndex((prev) => prev + (reverse ? -1 : 1));
+    }, Math.max(reverse ? 75 : subIndex === words[index].length ? 1000 : 150, parseInt(Math.random() * 350)));
+
+    return () => clearTimeout(timeout);
+  }, [subIndex, index, reverse]);
+
+  // blinker
+  useEffect(() => {
+    const timeout2 = setTimeout(() => {
+      setBlink((prev) => !prev);
+    }, 500);
+    return () => clearTimeout(timeout2);
+  }, [blink]);
+
+  return (
+    <Container className="plr0" fluid>
+      <Container fluid className="pbottom125 plr0">
+        <Jumbotron
+          className="height"
+          fluid
+          style={{
+            backgroundImage: `url(${bgimage})`,
+            backgroundSize: "cover",
+          }}
+        >
+          <Container fluid className="align">
+            <Row className="justify-content-md-center">
+              <Col>
+                <h1 className="animated-h1">
+                  Welcome to{" "}
+                  {`${words[index].substring(0, subIndex)}${blink ? "" : " "}`}
+                </h1>
               </Col>
             </Row>
           </Container>
-        </Container>
-      </Jumbotron>
-    </Container>
-    <Container fluid className="ptop125 pbottom125 paddinglr">
-      <Container fluid className=" ptop125 pbottom125 boxshadow">
-        <Row>
-          <Col className="paddingLrInner">
-            <Jumbotron className="bgColorWhite" fluid>
-              <Container fluid>
-                <Container fluid>
-                  <h1>Balu</h1>
-                </Container>
-                <Container fluid>
-                  <p className="ptop paratext">
-                    Balu Technologies is a team of award winning designers and
-                    software engineers. We work with startups, scaleups and
-                    multinational corporates to design, develop and deploy
-                    mission-led projects.
-                  </p>
-                </Container>
-              </Container>
-            </Jumbotron>
-          </Col>
-
-          <Col className="paddingLrInner">
-            <Image
-              src="https://images.unsplash.com/photo-1564069114553-7215e1ff1890?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=4178&q=80"
-              rounded
-            />
-          </Col>
-        </Row>
+        </Jumbotron>
       </Container>
+      <div class="split-pane split-pane-copy" data-alternate="true">
+        <div class="split-pane-inner">
+          <div class="split-pane-left split-pane-copy-copy">
+            <div class="split-pane-copy-copy-inner">
+              <div class="rm-markdown">
+                <h3 style={{ fontWeight: "bold" }}>Introduction</h3>
+                <p>
+                  Balu Technologies is a close knit team of software engineers.
+                  We work with startups, scaleups and multinational corporates
+                  to develop and deploy projects.
+                </p>
+                <NavLink
+                  className="linkItems"
+                  activeClassName="active"
+                  to="/projects"
+                >
+                  <button
+                    data-is-icon-button="false"
+                    data-danger-level="1"
+                    type="button"
+                    name="Our People"
+                    aria-label="Our People"
+                    data-variant="brand-secondary"
+                    class="btn"
+                  >
+                    <p>More about us</p>
+                  </button>
+                </NavLink>
+              </div>
+            </div>
+          </div>
+          <div class="split-pane-right split-pane-copy-image">
+            <span class="lazy-image-wrapper">
+              <picture class="" data-loaded="true">
+                <img
+                  alt="home"
+                  class=""
+                  src="https://images.unsplash.com/photo-1564069114553-7215e1ff1890?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nnx8b2ZmaWNlfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+                />
+              </picture>
+            </span>
+          </div>
+        </div>
+      </div>
+      <div
+        class="split-pane split-pane-copy ptop125 pbottom125"
+        data-alternate="true"
+      >
+        <div class="split-pane-inner">
+          <div class="split-pane-left split-pane-copy-copy">
+            <div class="split-pane-copy-copy-inner">
+              <div class="rm-markdown">
+                <h3 style={{ fontWeight: "bold" }}>Our people</h3>
+                <p>
+                  We're a purpose-focused team that works hand-in-hand with our
+                  clients to bring their mission to life. We believe small,
+                  focused teams can deliver exceptional results, and deliver all
+                  our projects with our awesome permanent in-house staff.
+                </p>
+                <NavLink
+                  className="linkItems"
+                  activeClassName="active"
+                  to="/team"
+                >
+                  <button
+                    data-is-icon-button="false"
+                    data-danger-level="1"
+                    type="button"
+                    name="Our People"
+                    aria-label="Our People"
+                    data-variant="brand-secondary"
+                    class="btn"
+                  >
+                    <p>Our People</p>
+                  </button>
+                </NavLink>
+              </div>
+            </div>
+          </div>
+          <div class="split-pane-right split-pane-copy-image">
+            <span class="lazy-image-wrapper">
+              <picture class="" data-loaded="true">
+                <img
+                  alt="home"
+                  class=""
+                  src="https://images.unsplash.com/photo-1541746972996-4e0b0f43e02a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NHx8b2ZmaWNlfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+                />
+              </picture>
+            </span>
+          </div>
+        </div>
+      </div>
     </Container>
-    <Container fluid className="ptop125 pbottom125 paddinglr">
-      <Container fluid className=" ptop125 pbottom125 boxshadow">
-        <Row>
-          <Col className="paddingLrInner">
-            <Image
-              src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=3151&q=80"
-              rounded
-            />
-          </Col>
-          <Col className="paddingLrInner">
-            <Jumbotron className="bgColorWhite" fluid>
-              <Container fluid>
-                <Container fluid>
-                  <h1>Our People</h1>
-                </Container>
-                <Container fluid>
-                  <p className="ptop paratext">
-                    We're a purpose-focused team that works hand-in-hand with
-                    our clients to bring their mission to life. We believe
-                    small, focused teams can deliver exceptional results, and
-                    deliver all our projects with our awesome permanent in-house
-                    staff.
-                  </p>
-                </Container>
-              </Container>
-            </Jumbotron>
-          </Col>
-        </Row>
-      </Container>
-    </Container>
-  </Container>
-);
+  );
+};
 
 export default HomePage;
