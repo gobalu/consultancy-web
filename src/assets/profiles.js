be5ed91cecb10728f95cf50dac534f09978fecc3
Ben Jefferies <benjefferies@echosoft.uk>
2021-03-30 09:45:23 +0100

    Add cv links

--- src/assets/profiles.js
+++ src/assets/profiles.js
@@ -6,6 +6,7 @@ const profiles = [
     github: "https://github.com/benjefferies",
     linkedin: "https://www.linkedin.com/in/ben-jefferies-0bb1b24b/",
     strava: "https://www.strava.com/athletes/18464706/",
+    cv: "https://docs.google.com/document/d/1te5J5ZVG71lXGhlR-4qIBC9stZsRLcKbwAqsLu8I0LM/edit?usp=sharing",
     profilePicture: "../assets/images/bio/ben-profile.jpg",
     sideImage: "../assets/images/bio/ben-side.jpg",
   },
@@ -14,7 +15,8 @@ const profiles = [
     description:
       "Jack is an experienced Software Engineer, originally a Business Intelligence apprentice, he's transitioned to a full stack developer. He has a wealth of experience in Dev-ops, Frontend, Backend, and Mobile Development from the past 8 years. He has worked on a handful of open-source project that are available to view on his GitHub. In his spare time, he loves gaming, and playing 5 a side football with his friends.",
     github: "https://github.com/DaJaffaMan",
-    linkedin: "https://www.linkedin.com/in/jack-jefferies-0b904a99/",
+    linkedin: "https://docs.google.com/document/d/1p0ume2JrkXyK-aZreh6JZL1hZPpuIuiZwWq1fuv8Pwg/edit?usp=sharing",
+    cv: "https://www.strava.com/athletes/18464706/",
     profilePicture: "../assets/images/bio/jack-profile.jpg",
     sideImage: "../assets/images/bio/jack-side.jpg",
   },
--- src/components/Bio.js
+++ src/components/Bio.js
@@ -8,70 +8,107 @@ import Row from "react-bootstrap/Row";
 import github from "../assets/github-icon.png";
 import linkedin from "../assets/linkedin-icon.png";
 import strava from "../assets/strava-icon.png";
+import cv from "../assets/cv-icon.png";
 
 const Bio = (props) => {
   return (
     <>
       <Container className="bio">
         <Row className="justify-content-xs-center">
-          <Col xs={3} style={{
-            display: "flex",
-            alignItems: "center",
-          }}>
-            <Container style={{
-              display: "flex",
-              justifyContent: "center"
-            }}>
-              <img style={styles.bioProfile} src={props.person.profilePicture} alt="profile"/>
-            </Container>
-          </Col>
-          <Col xs={4} style={{
-            paddingTop: 25,
-            paddingBottom: 25
-          }}>
-            <Container>
-              <h2>{props.person.name}</h2>
-              <p style={{
-                paddingTop: 10,
-                paddingBottom: 25
-              }}>{props.person.description}</p>
-            </Container>
-            <Container style={{
-              display: "flex",
-              alignItems: "center"
-            }}>
-              <a href={props.person.github}><Image src={github} style={styles.bioLinks} /></a>
-              <a href={props.person.linkedin}><Image src={linkedin} style={styles.bioLinks} /></a>
-              {props.person.strava && <a href={props.person.strava}><Image src={strava} style={styles.bioLinks} /></a>}
-            </Container>
-          </Col>
-          <Col xs={5} style={{
-            display: "flex",
-            alignItems: "center",
-          }}>
-            <Container style={{
+          <Col
+            xs={3}
+            style={{
               display: "flex",
               alignItems: "center",
-            }}>
+            }}
+          >
+            <Container
+              style={{
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <img
+                style={styles.bioProfile}
+                src={props.person.profilePicture}
+                alt="profile"
+              />
+            </Container>
+          </Col>
+          <Col
+            xs={4}
+            style={{
+              paddingTop: 25,
+              paddingBottom: 25,
+            }}
+          >
+            <Container>
+              <h2>{props.person.name}</h2>
+              <p
+                style={{
+                  paddingTop: 10,
+                  paddingBottom: 25,
+                }}
+              >
+                {props.person.description}
+              </p>
+            </Container>
+            <Container
+              style={{
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <a href={props.person.linkedin}>
+                <Image src={linkedin} style={styles.bioLinks} />
+              </a>
+              {props.person.cv && (
+                <a href={props.person.cv}>
+                  <Image src={cv} style={styles.bioLinks} />
+                </a>
+              )}
+              <a href={props.person.github}>
+                <Image src={github} style={styles.bioLinks} />
+              </a>
+              {props.person.strava && (
+                <a href={props.person.strava}>
+                  <Image src={strava} style={styles.bioLinks} />
+                </a>
+              )}
+            </Container>
+          </Col>
+          <Col
+            xs={5}
+            style={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <Container
+              style={{
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
               <Image src={props.person.sideImage} rounded />
             </Container>
           </Col>
         </Row>
       </Container>
     </>
-  )
+  );
 };
 
 const styles = {
   bioLinks: {
     maxWidth: 24,
     marginLeft: 8,
-    marginRight: 8
+    marginRight: 8,
   },
   bioProfile: {
     maxWidth: 128,
     borderRadius: 64,
-  }
-}
+  },
+};
 
 export default Bio;
